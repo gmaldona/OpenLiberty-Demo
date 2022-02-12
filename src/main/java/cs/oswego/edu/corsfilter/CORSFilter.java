b3ea80705e47cc6b717722756302b0d23b5c7ec9
jannyr08 <pokada816@gmail.com>
2022-02-12 15:48:46 -0500

    fixed CORS issue

--- src/main/java/cs/oswego/edu/corsfilter/CORSFilter.java
+++ src/main/java/cs/oswego/edu/corsfilter/CORSFilter.java
@@ -13,7 +13,6 @@ public class CORSFilter implements ContainerResponseFilter {
    @Override
    public void filter(final ContainerRequestContext requestContext,
                       final ContainerResponseContext cres) throws IOException {
-      cres.getHeaders().add("Access-Control-Allow-Origin", "*");
       cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
       cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
       cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");

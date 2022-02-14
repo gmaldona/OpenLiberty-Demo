06e8eb0ac8ac13c875f4403d795b53250c22610f
Mint <developer@chocomint.dev>
2022-02-13 22:32:23 -0500

    Added comments

--- mongo-init.js
+++ mongo-init.js
@@ -1,3 +1,5 @@
+// Edit your user, pwd, and db variables that you want your database to be initialized with when running `docker-compose.yml`.
+// Make sure the variables entered here also match the ones in your src/main/webapp/META-INF/microprofile-config.properties file.
 db.createUser(
     {
         user: "",
